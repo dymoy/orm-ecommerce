2558b33900a02e332e7b8f29d652203b2be57ea1
dymoy <148160802+dymoy@users.noreply.github.com>
2024-02-06 15:57:03 -0800

    Completed product-routes 'get /', 'get /:id', 'post /', 'delete /:id'.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -99,7 +99,7 @@ router.delete('/:id', async (req, res) => {
 
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json('The requested category id was not found.');
+    res.status(500).json(err);
   }
 });
 
