22856e096aa49795c539ee2cf18cf26192bbe24d
dymoy <148160802+dymoy@users.noreply.github.com>
2024-02-07 16:32:01 -0800

    Completed 'put /:id' and 'delete /:id' api routes for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,37 +52,100 @@ router.get('/:id', async (req, res) => {
 });
 
 // Create a new tag 
-router.post('/', (req, res) => {
-  // Use the .create() method from Tag Model to create the tag instance 
-  Tag.create({
-    tag_name: req.body.tag_name,
-    product_ids: req.body.product_ids
-  }).then((tag) => {
-    // If there are product ids, create pairings to bulkCreate in the ProductTag Model
-    if (req.body.product_ids.length) {
-      const productTagIdArr = req.body.product_ids.map((product_id) => {
-        return {
-          product_id,
-          tag_id: tag.id
-        }
-      });
-      return ProductTag.bulkCreate(productTagIdArr);
-    }
-    // If ther eare no product ids, respond with the created tag 
-    res.status(200).json(tag);
-  }).then((productTagIds) => {
-    res.status(200).json(productTagIds);
-  }).catch((err) => {
+router.post('/', async (req, res) => {
+  try { 
+    // Use the .create() method from Tag Model to create the tag instance 
+    await Tag.create({
+      tag_name: req.body.tag_name,
+      product_ids: req.body.product_ids
+    }).then((tag) => {
+      // If there are product ids, create pairings to bulkCreate in the ProductTag Model
+      if (req.body.product_ids.length) {
+        const productTagIdArr = req.body.product_ids.map((product_id) => {
+          return {
+            product_id,
+            tag_id: tag.id
+          }
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // If there are no product ids, respond with the created tag 
+      res.status(200).json(tag);
+    }).then((productTagIds) => {
+      res.status(200).json(productTagIds);
+    });
+  } catch(err) {
     res.status(500).json(err);
-  });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// Update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  // Use the .update() method in Tag Model 
+  try {
+    await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    }).then((tag) => {
+      // If there are product ids provided in the req.body, update the products as well in ProductTag Model
+      if (req.body.product_ids && req.body.product_ids.length) {
+        // Find all productTag pairings with the tag_id we're updating 
+        ProductTag.findAll({
+          where: {tag_id: req.params.id}
+        }).then((productTags) => {
+          // Create filtered lists of new product_ids
+          const productTagIds = productTags.map(({product_id}) => product_id);
+  
+          const newProductTags = req.body.product_ids
+          .filter((product_id) => !productTagIds.includes(product_id))
+          .map((product_id) => {
+            return {
+              product_id,
+              tag_id: req.params.id
+            }
+          });
+          
+          // Remove any productTags that are not included in the req.body
+          const productTagsToRemove = productTags
+          .filter(({product_id}) => !req.body.product_ids.includes(product_id))
+          .map(({id}) => id);
+  
+          return Promise.all([
+            ProductTag.destroy({
+              where: {
+                id: productTagsToRemove
+              }
+            }),
+            ProductTag.bulkCreate(newProductTags)
+          ]);
+        });
+      }
+      return res.status(200).json(tag);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// Delete a Tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({
+        message: 'The requested tag id was not found.'
+      });
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
